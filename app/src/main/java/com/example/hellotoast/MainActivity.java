796b7a42cd76d82cffbbea49372692b3f0a8850c
elgatovielma <vielmagalberto@gmail.com>
2018-05-09 00:00:56 -0400

    prueba de entrar

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -11,9 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
-    //mamamamamasmsmasmsamasmasmmsaasmasmsamasmas
-    //asdasaads
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
