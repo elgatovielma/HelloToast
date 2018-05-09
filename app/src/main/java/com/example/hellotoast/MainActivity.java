e790b0442f1b98aff35c1f808ebce8a39b17a490
elgatovielma <vielmagalberto@gmail.com>
2018-05-08 23:21:19 -0400

    ssss

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    //mamamamamasmsmasmsamasmasmmsaasmasmsamasmas
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
